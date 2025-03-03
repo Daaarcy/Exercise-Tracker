f9cbf6a4bb5c2c506b0c77066eed19842b0d09c7
Ching-fung WAN <cfwan6@gmail.com>
2025-03-03 15:40:38 +0000

    create data

--- index.js
+++ index.js
@@ -9,7 +9,48 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+let users = [];
 
+// create new user
+app.post("/api/users", function(req, res){
+  let userName = req.username;
+
+  users.push({ username: userName, _id: users.lenght + 1 });
+  res.json({ username: userName, _id: users.lenght + 1 });
+})
+
+// a list of all users
+app.get("/api/users/:_id/logs", function(req, res){
+  res.send(users)
+})
+
+// form description, duration, and optionally date data
+app.post("/api/users/:_id/exercises", function(req, res){
+  let description = req.description;
+  let duration = req.duration;
+  let date = req.date;
+
+  // If no date is supplied, the current date will be used.
+  if(!date){
+    date = new Date();
+  };
+
+  users.push({ 
+    username: userName,
+    description: description,
+    duration: duration,
+    date, date.toDateString(),
+     _id: users.lenght + 1 });
+
+  res.json({ 
+    username: userName,
+    description: description,
+    duration: duration,
+    date, date,
+     _id: users.lenght + 1 });
+
+})
+//
 
 
 
